f8b4e1286eb843dbbcf3272c5187de2e8554d545
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-09-20 19:58:43 -0300

    basics for all options

--- __init__.py
+++ __init__.py
@@ -4,22 +4,32 @@ import trello
 options = {
   "boards": {
       "create": trello.default ,
+      "ls": trello.default ,
+      "delete": trello.default
   },
   "cards": {
-
+      "create": trello.default ,
+      "ls": trello.default ,
+      "delete": trello.default ,
+      "comment": trello.default ,
+      "categorize": trello.default
   },
   "labels": {
-      
+      "create": trello.default ,
+      "ls": trello.getLabels ,
+      "delete": trello.default
   },
   "lists": {
-
+      "create": trello.default ,
+      "ls": trello.getLists ,
+      "delete": trello.default
   }
 }
 
 args = sys.argv
 
 # check for first option - entity
-if args[1] in options.keys():
+if (len(args) > 1) and (args[1] in options.keys()):
     opt1 = options[args[1]]
 else:
     print("Sorry, we don't have this option. Instead, try to use: ")
@@ -28,7 +38,7 @@ else:
     quit()
 
 # check for second option - method
-if args[2] in opt1.keys():
+if (len(args) > 2) and (args[2] in opt1.keys()):
     opt2 = opt1[args[2]]
 else:
     print("Sorry, we don't have this option. Instead, try to use: ")
@@ -36,4 +46,4 @@ else:
         print(args[1] + " " +key + " -- options")
     quit()
 
-opt2()
+print(opt2(args[3:]))
