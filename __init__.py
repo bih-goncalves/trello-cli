7c0a867f8b14fb88ecfcfd4eb2f9afcf364df869
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-09-20 19:33:35 -0300

    definition of options for cli

--- __init__.py
+++ __init__.py
@@ -1,4 +1,33 @@
+import sys
 import trello
+
+options = {
+  "boards": {
+      "create": trello.default ,
+  },
+  "cards": {
+
+  },
+  "labels": {
+      
+  },
+  "lists": {
+
+  }
+}
+
+args = sys.argv
+
+if args[1] in options.keys():
+    opt1 = options[args[1]]
+else:
+    print("Sorry, we don't have this option. Instead, try to use: ")
+    for key in options:
+        print(key + " -- options")
+    quit()
+
+opt2 = opt1[args[2]]
+
 
 
 print(trello.getLists('bS7VxUXP'))
