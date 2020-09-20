6bbb628056e4d6b561511b399f48e6e79fe89172
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-09-20 19:58:59 -0300

    feat: labels list

--- trello.py
+++ trello.py
@@ -11,7 +11,10 @@ def default():
 # list functions
 
 def getLists(board_id):
-    url = f"https://api.trello.com/1/boards/{board_id}/lists"
+    if len(board_id) < 1:
+        return "Missing param. Please give the board ID"
+
+    url = f"https://api.trello.com/1/boards/{board_id[0]}/lists"
 
     query = {"key": key, "token": token}
 
@@ -25,6 +28,23 @@ def getLists(board_id):
     return lists
 
 # label functions
+
+def getLabels(board_id):
+    if len(board_id) < 1:
+        return "Missing param. Please give the board ID"
+
+    url = f"https://api.trello.com/1/boards/{board_id[0]}/labels"
+
+    query = {"key": key, "token": token}
+
+    response = requests.request(
+        "GET", url,
+        params=query
+        )
+
+    labels = response.json()
+
+    return labels
 
 # card functions
 
