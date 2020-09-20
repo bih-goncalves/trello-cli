04ecb20455926382da7f8cf33f3c1005ef32a384
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-09-20 20:54:10 -0300

    feat: create card

--- trello.py
+++ trello.py
@@ -63,7 +63,62 @@ def addLabels(args):
 
     return label
 
+###### card functions
 
+def createCard(args):
+    # check if we need label
+    if '-l' in args:
+        index = args.index('-l')
+        args.pop(index) # remove the option
+        label = args.pop(index) # get value of label
+
+        # check if the label exist
+        labels = getLabels([args[0]])
+        l3 = []
+        for item in labels:
+            l3.append(item['name'])
+        
+        if label not in l3:
+            return "Sorry, this labels doesn't exist. Please create before using."
+        else:
+            label = labels[l3.index(label)]['id']
+
+    #check if we have comment
+    if '-c' in args:
+        index = args.index('-c')
+        args.pop(index) # remove the option
+        comment = args.pop(index) # get value of comment
+
+    if len(args) < 4:
+        return "Missing param. Please give the board ID, list number, card name and card description"
+
+    # check the list ID
+    lists = getLists([args[0]])
+    if len(lists) >= int(args[1]):
+        list_id = lists[int(args[1])]['id']
+    else:
+        return "This list doesn't exist. Please create before using."
+
+    url = f"https://api.trello.com/1/cards"
+
+    query = {"name": args[2], "desc": args[3], "idList": list_id, "key": key, "token": token}
+
+    response = requests.request(
+        "POST", url,
+        params=query
+        )
+
+    card_id = response.json()["id"]
+
+    # add label
+    if 'label' in locals():
+        addLabels([card_id, label])
+
+    # add comment
+    if 'comment' in locals():
+        addComment([card_id, comment])
+
+    return card_id
 
 
 ###### comment functions
